a7630b6aa5b2d14ac57e75f9f3911b5a30d524c2
Arjun <bairiarjunmudhiraj@gmail.com>
2026-07-24 21:36:27 +0530

    Day_11

---
+++ Day_11/palindrome.py
@@ -0,0 +1,14 @@
+def pal_fun(s,l,r):
+    n = len(s)
+    if l >= r:
+        return True
+    if s[l] != s[r]:
+        return False
+    return pal_fun(s,l+1,r-1)
+
+
+s = "hello"
+l=0
+r=len(s)-1
+
+print(pal_fun(s,l,r))
