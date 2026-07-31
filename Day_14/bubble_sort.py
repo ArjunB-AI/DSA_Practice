7a666cd63b49cf3ca979ba64ce49d5bece22c2fa
Arjun <bairiarjunmudhiraj@gmail.com>
2026-07-31 22:30:21 +0530

    added the bubble sort code for descending order

--- Day_14/bubble_sort.py
+++ Day_14/bubble_sort.py
@@ -1,3 +1,4 @@
+# Ascending order
 def bub_sort(num):
 
     n = len(num)
@@ -6,6 +7,18 @@ def bub_sort(num):
         for j in range(0,i+1):
             if num[j] > num[j+1]:
                 num[j],num[j+1] = num[j+1],num[j]
+    return num
+
+# Descending order
+def bub_sort(num):
+    
+    n = len(num)
+    
+    for i in range(n-2,-1,-1):
+        for j in range(0,i+1):
+            if num[j] < num[j+1]:
+                num[j],num[j+1] = num[j+1],num[j]
+        
     return num
 
 arr = [1,3,6,4,2,5]
