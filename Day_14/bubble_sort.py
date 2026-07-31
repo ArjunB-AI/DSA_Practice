8b2f72e426ff239ec3820e2dca1c53c26bb026ce
Arjun <bairiarjunmudhiraj@gmail.com>
2026-07-31 22:26:46 +0530

    Day_14

---
+++ Day_14/bubble_sort.py
@@ -0,0 +1,12 @@
+def bub_sort(num):
+
+    n = len(num)
+
+    for i in range(n-2,-1,-1):
+        for j in range(0,i+1):
+            if num[j] > num[j+1]:
+                num[j],num[j+1] = num[j+1],num[j]
+    return num
+
+arr = [1,3,6,4,2,5]
+print(bub_sort(arr))
