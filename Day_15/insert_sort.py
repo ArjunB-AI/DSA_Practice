26fc589ce4ccee6bd1c9842ca388fae74869ef09
Arjun <bairiarjunmudhiraj@gmail.com>
2026-08-01 15:36:37 +0530

    Day_15

---
+++ Day_15/insert_sort.py
@@ -0,0 +1,16 @@
+def insert_sort(nums):
+    n = len(nums)
+    for i in range(1,n):
+        key = nums[i]
+        j = i-1
+
+        while j >= 0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            j -= 1
+
+        nums[j+1] = key
+
+    return nums
+
+arr = [1,3,5,4,2]
+print(insert_sort(arr))
